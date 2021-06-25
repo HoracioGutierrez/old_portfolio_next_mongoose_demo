176c5dd4e3f33e72bcf5cb0d34fb1a18f3a3be20
HoracioGutierrez <horacio.estevez@gmail.com>
2021-06-25 17:08:11 -0300

    migrated basic title styling from old project

--- components/CardHeader/index.js
+++ components/CardHeader/index.js
@@ -11,7 +11,7 @@ const CardHeader = () => {
     return (
         <header className={style.cardHeader}>
             <div id="overlay" className={`${style.overlay} ${dark ? style.dark : style.light }`}></div>
-            <button className="material-icons">close</button>
+            <button className={`material-icons ${style.button}`}>close</button>
             <CardAvatar />
             <CardTitle />
             <SocialNavBar />
