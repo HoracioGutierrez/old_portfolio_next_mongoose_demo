c0acf3b98c062ffd05b8379479ee45d46141af68
HoracioGutierrez <horacio.estevez@gmail.com>
2021-06-26 19:41:03 -0300

    finish second working draft

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,9 @@ const Landing = () => {
             <Head>
                 <title>HG - Home</title>
             </Head>
-            <h1>Landing</h1>
+            <p>Soy Horacio Gutierrez, tengo 31 años y vivo en Argentina. Me gusta mucho programar en Javascript, tocar la bateria en mi imaginacion mientras golpeo los dedos contra algo y jugar Valorant. Esta página la hice a modo de tener un portfolio online por un lado, pero por el otro también como playground personal para probar distintas cosas y ver como quedan en producción.</p>
+            <br />
+            <p>Podes ver en la sección de "Demos" cada funcionalidad que voy probando pero no prometo que todas sean buenas o realmente útiles.</p>
         </>
     )
 }
