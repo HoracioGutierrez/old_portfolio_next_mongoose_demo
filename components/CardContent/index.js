421dd54965b4bb2cf0691a063c6c3adaa955edeb
HoracioGutierrez <horacio.estevez@gmail.com>
2021-06-26 19:27:11 -0300

    Polished tabs styling and add routing click with push

--- components/CardContent/index.js
+++ components/CardContent/index.js
@@ -1,17 +1,45 @@
 import { Collapse } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import CardContent from '@material-ui/core/CardContent';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import style from "./CardContent.module.scss"
+import { useState } from "react";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useRouter } from 'next/router'
 
-export default  ({children}) => {
+const Content = ({ children }) => {
 
-    const open = useSelector(({card_open})=>card_open)
+    const open = useSelector(({ card_open }) => card_open)
+    const [tab, setTab] = useState(0)
+    const matches = useMediaQuery("(min-width:800px)")
+    const router = useRouter()
+    const [rutas] = useState(["/", "/demos"])
+
+
+    const handleChange = (e, tab) => {
+        setTab(tab)
+        const ruta = rutas[tab]
+        router.push(ruta)
+    }
 
     return (
         <Collapse in={!open}>
+            <Tabs
+                value={tab}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                variant={!matches ? "fullWidth" : "standard"}
+            >
+                <Tab label="Bio" id="bio" className={style.tabItem} />
+                <Tab label="Demos" id="demos" />
+            </Tabs>
             <CardContent component="main" className={style.cardContent}>
                 {children}
             </CardContent>
         </Collapse>
     );
 }
+
+export default Content
---
+++ pages/demos.js
@@ -0,0 +1,7 @@
+const Demos = () => {
+    return (
+        <h1>Demos</h1>
+    )
+}
+ 
+export default Demos
